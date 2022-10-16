27c9e4e9925809c3f64be172a15d29e150086fdf
Habeba Hamada <99296931+HabebaHamada@users.noreply.github.com>
2022-10-16 14:38:48 +0200

    Add files via upload

---
+++ stm32f401x/HAL/2-74HC595/ShiftReg_interface.h
@@ -0,0 +1,41 @@
+/*************************************************************/
+/*************       Author: Habeba Hamada    ****************/
+/*************       LAYER: HAL               ****************/
+/*************       Component: Shift Register ***************/
+/*************       Version: 1.00            ****************/
+/*************************************************************/
+
+
+
+#ifndef _SHIFTREG_INTERFACE_H_
+#define _SHIFTREG_INTERFACE_H_
+
+
+
+typedef struct ShiftRegister ShiftRegister;
+
+struct ShiftRegister
+{
+	u8 Data_Port;
+	u8 Data_Pin;
+	u8 Shift_Clock_Port;
+	u8 Shift_Clock_Pin;
+	u8 Latch_Clock_Port;
+	u8 Latch_Clock_Pin;
+	u8 Output_Enable_Port;
+	u8 Output_Enable_Pin;
+};
+
+
+/*****************
+ * Name        : ShiftRegister_voidSendData        *
+ * Return type : void                              *
+ * Arguments   : Copy_ShiftReg  --> ShiftRegister  *
+ *               Copy_u8Data    --> u8             *
+ * description : Sends Data to Shift Register      *
+ *****************/
+void ShiftRegister_voidSendData(ShiftRegister Copy_ShiftReg, u8 Copy_u8Data);
+void ShiftReg_voidInit(void);
+
+
+#endif
